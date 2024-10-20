071c76dee0518cfd5d7641b68f529debd9c164c4
Navya <88372644+Navya-Raj@users.noreply.github.com>
2024-10-20 15:25:28 +0530

    added frontend

---
+++ mercari-herspace/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Layouts/Navbar';
+import Home from './Components/Pages/Home';
+import FloatingChatIcon from './Components/FloatingChatIcon';
+import Footer from './Components/Layouts/Footer';
+
+
+
+
+
+
+// import YouTubeEvents from './YouTubeEvents';
+
+import  {AuthProvider}  from './AuthContext';
+// testing pull works 
+
+function App() {
+  return (
+    <AuthProvider>
+    <Router>
+      <Navbar />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        
+          
+          {/* <Route path="/events" element={<YouTubeEvents />} /> */}
+        </Routes>
+        <FloatingChatIcon />
+
+      </div>
+     
+    </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
